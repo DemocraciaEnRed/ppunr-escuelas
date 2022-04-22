2982e35224c6d9020732033ff4a4d5fd827ebbfc
Guillermo Croppi <guillermocroppi@gmail.com>
2022-04-22 12:54:46 -0300

    Album rework

--- ext/lib/site/formulario-propuesta/formulario-propuesta.js
+++ ext/lib/site/formulario-propuesta/formulario-propuesta.js
@@ -1,6 +1,7 @@
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
 import CrearPropuesta from './component'
+import AlbumPropuesta from './componentAlbum'
 
 router.childRoutes.unshift({
   path: 'formulario-idea',
@@ -10,4 +11,8 @@ router.childRoutes.unshift({
 router.childRoutes.unshift({
   path: 'formulario-idea/:id',
   component: CrearPropuesta
+})
+router.childRoutes.unshift({
+  path: 'formulario-idea/:id/album',
+  component: AlbumPropuesta
 })
