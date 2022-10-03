dc7f38c1bd672f8ca98a2bfac4b129cea9ce9a9b
Mariano Valdez <marianovaldez92@protonmail.com>
2022-10-03 17:10:41 -0300

    Cambio letsGoUp

--- letsGoUp.sh
+++ letsGoUp.sh
@@ -1,7 +1,7 @@
 # Arriba el docker de mongo
-docker ps -a | grep mongodb-der | docker start mongodb-der || docker run --name "mongodb-der" -p 27017:27017 --volume /tmp/db:/data/db -w /data/db -d mongo:3.2
+docker ps -a | grep mongodb-der | docker start mongodb-der || docker run --name "mongodb-der" -p 27017:27017 --volume `pwd`/tmp/db:/data/db -w /data/db -d mongo:3.2
 
-# # Uso node v8
+# Uso node v8
 source ~/.nvm/nvm.sh
 nvm use v8.17.0
 
