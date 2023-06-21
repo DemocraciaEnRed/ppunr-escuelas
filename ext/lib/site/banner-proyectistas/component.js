ba8ab3ac46f95486103d99514d7dc888d139749f
xtian7489 <xtian7489@gmail.com>
2023-06-21 11:52:21 -0300

    fix text banner proyectista

--- ext/lib/site/banner-proyectistas/component.js
+++ ext/lib/site/banner-proyectistas/component.js
@@ -122,7 +122,7 @@ class BannerProyectistas extends Component {
         </div>
       )}
       <div>
-      <p className="help-text"><i className="icon-info icon-large"></i> <u>Ser proyectista</u> implica transformar las ideas en proyectos. <br/>Podes informarte más acerca de que es ser proyectista en la sección de <Link href="/acerca-de">Acerca De</Link></p>      </div>
+      <p className="help-text"><i className="icon-info icon-large"></i> <u>Ser proyectista</u> implica transformar las ideas en proyectos. <br/>Podes informarte más acerca de que es ser proyectista en la sección de <Link href="/acerca-de">Información</Link></p>      </div>
     </div>
 
    )
